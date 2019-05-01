bf44063462be1e1b70063497962778676783a2c3
William Clifford <williamdavidclifford@gmail.com>
2019-05-01 14:12:04 +1000

    Form and listing creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  get 'listing/index'
-  get 'listing/show'
-  get 'listing/new'
-  get 'listing/create'
-  get 'listing/edit'
-  get 'home/index'
   devise_for :users
+  get 'home/index'
+  # get 'listing/index'
+  # get 'listing/show'
+  # get 'listing/new'
+  # get 'listing/create'
+  # get 'listing/edit'
+  resources :listings
+
+  # post 'listing/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "listing#index"
+  root "listings#index"
 end
