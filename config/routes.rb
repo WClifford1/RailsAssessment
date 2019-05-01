521a2f78ba46337c81402fa0ea524f8ea146c7ce
William Clifford <williamdavidclifford@gmail.com>
2019-05-01 11:40:53 +1000

    Route change

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'listing/new'
   get 'listing/create'
   get 'listing/edit'
-  get 'listing/index'
+  get 'home/index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "listing#index"
