6193449aa99684d105ce53ed8755805272800932
William Clifford <williamdavidclifford@gmail.com>
2019-05-03 12:09:57 +1000

    stripe

--- db/schema.rb
+++ db/schema.rb
@@ -50,6 +50,19 @@ ActiveRecord::Schema.define(version: 2019_05_03_003313) do
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
+  create_table "models", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "order"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_models_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+  end
+
   create_table "orders", force: :cascade do |t|
     t.string "item", null: false
     t.decimal "price", precision: 15, scale: 2, null: false
