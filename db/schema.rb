171e40bd20ac2bb7749e53b350754daf554759eb
William Clifford <williamdavidclifford@gmail.com>
2019-05-10 15:03:48 +1000

    removed unsued models and tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_10_002542) do
+ActiveRecord::Schema.define(version: 2019_05_10_045633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,19 +57,6 @@ ActiveRecord::Schema.define(version: 2019_05_10_002542) do
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
-  create_table "models", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "order"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_models_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
-  end
-
   create_table "orders", force: :cascade do |t|
     t.bigint "listing_id"
     t.datetime "created_at", null: false
@@ -78,18 +65,6 @@ ActiveRecord::Schema.define(version: 2019_05_10_002542) do
     t.integer "quantity", default: 1
     t.index ["cart_id"], name: "index_orders_on_cart_id"
     t.index ["listing_id"], name: "index_orders_on_listing_id"
-  end
-
-  create_table "profiles", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "avatar"
-    t.string "city"
-    t.string "country"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "roles", force: :cascade do |t|
@@ -131,5 +106,4 @@ ActiveRecord::Schema.define(version: 2019_05_10_002542) do
   add_foreign_key "listings", "users"
   add_foreign_key "orders", "carts"
   add_foreign_key "orders", "listings"
-  add_foreign_key "profiles", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,1 @@
 
-
-Listing.create!([
-  {title: "seed", price: "1.0", description: "seed", address: "seed", city: "seed", state: "seed", country: "seed", user_id: 1},
-  {title: "Macbook Pro", price: "1300.0", description: "Definitely better than windows", address: "123 Foo Bar Street", city: "City of Foo", state: "State of Bar", country: "Australia", user_id: 2},
-  {title: "Headphones", price: "5000.0", description: "An amazing set of headphones", address: "44 Street Road", city: "Sydney", state: "NSW", country: "Australia", user_id: 2}
-])
