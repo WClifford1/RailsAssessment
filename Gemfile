9af980b51f373027db1e7a39981b8f16271e9e40
William Clifford <williamdavidclifford@gmail.com>
2019-05-10 14:22:31 +1000

    add seed dump gem

--- Gemfile
+++ Gemfile
@@ -15,7 +15,7 @@ gem 'stripe', :git => 'https://github.com/stripe/stripe-ruby'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
 
-
+gem 'seed_dump'
 gem 'rolify'
 gem "activestorage-cloudinary-service", "~> 0.2.3"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+Listing.create!([
+  {title: "seed", price: "1.0", description: "seed", address: "seed", city: "seed", state: "seed", country: "seed", user_id: 1},
+  {title: "Macbook Pro", price: "1300.0", description: "Definitely better than windows", address: "123 Foo Bar Street", city: "City of Foo", state: "State of Bar", country: "Australia", user_id: 2},
+  {title: "Headphones", price: "5000.0", description: "An amazing set of headphones", address: "44 Street Road", city: "Sydney", state: "NSW", country: "Australia", user_id: 2}
+])
