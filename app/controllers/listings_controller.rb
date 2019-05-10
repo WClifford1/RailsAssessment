c0c1282d7c061b20c633b61aa8b8d895ade4a6d2
William Clifford <williamdavidclifford@gmail.com>
2019-05-10 15:43:17 +1000

    remove unsed code in listings controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -40,8 +40,6 @@ class ListingsController < ApplicationController
   def destroy
     @listing.destroy
     redirect_to listings_path, notice: 'Listing successfully deleted'
-    @listing.image.destroy
-    redirect_to listings_path, notice: 'Listing successfully deleted'
 end
 
 end
