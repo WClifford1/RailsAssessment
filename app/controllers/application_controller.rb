bb87da01241712e22658c42f92d2bf13aa5330fb
fari3001 <fari3001_14@hotmail.com>
2019-05-10 12:52:58 +1000

    Not Working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     include CurrentCart
-    before_action :set_cart
+    
+    
 
     before_action :configure_permitted_parameters, if: :devise_controller?
 
